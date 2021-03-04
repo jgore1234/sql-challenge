085f7324399ca2a1e6230fc925a657939404c98e
John Gore <johngore@Johns-MBP.lan>
2021-03-03 20:31:29 -0800

    upload files

---
+++ schema.sql
@@ -0,0 +1,97 @@
+﻿-- Create the titles table, with a primary id and a title
+
+
+
+-- Create an employees table, with the following: 
+--    employee number, 
+--    employee title id,
+--    birth date
+--    first name
+--    last name
+--    sex
+--    hire date
+-- set the foreign key and primary key relationships accordingly
+CREATE TABLE "Employees" (
+    "emp_no" INTEGER   NOT NULL,
+    "emp_title_id" VARCHAR   NOT NULL,
+    "birth_date" DATE   NOT NULL,
+    "first_name" VARCHAR   NOT NULL,
+    "last_name" VARCHAR   NOT NULL,
+    "sex" VARCHAR   NOT NULL,
+    "hire_date" DATE   NOT NULL,
+    CONSTRAINT "pk_Employees" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+CREATE TABLE "Titles" (
+    "title_id" VARCHAR   NOT NULL,
+    "title" VARCHAR   NOT NULL
+);
+
+-- Create an salaries table, with the following: 
+--    salary 
+--    employee number
+-- set the foreign key and primary key relationships accordingly
+
+
+CREATE TABLE "Salaries" (
+    "emp_no" INTEGER   NOT NULL,
+    "salary" INTEGER   NOT NULL,
+    CONSTRAINT "pk_Salaries" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+-- Create an dept_emp table, with the following: 
+--    department number, 
+--    employee number
+-- set the foreign key and primary key relationships accordingly
+
+CREATE TABLE "Department_emp" (
+    "emp_no" INTEGER   NOT NULL,
+    "dept_no" VARCHAR   NOT NULL,
+    CONSTRAINT "pk_Department_emp" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+-- Create an departments table, with the following: 
+--    department number, 
+--    department name
+-- set the primary key relationships accordingly
+CREATE TABLE "Department_Manager" (
+    "dept_no" VARCHAR   NOT NULL,
+    "emp_no" INTEGER   NOT NULL,
+    CONSTRAINT "pk_Department_Manager" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+-- Create an departments table, with the following: 
+--    department number, 
+--    department name
+-- set the primary key relationships accordingly
+CREATE TABLE "Departments" (
+    "dept_no" VARCHAR   NOT NULL,
+    "dept_name" VARCHAR   NOT NULL
+);
+
+ALTER TABLE "Employees" ADD CONSTRAINT "fk_Employees_emp_title_id" FOREIGN KEY("emp_title_id")
+REFERENCES "Titles" ("title_id");
+
+ALTER TABLE "Salaries" ADD CONSTRAINT "fk_Salaries_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "Employees" ("emp_no");
+
+ALTER TABLE "Department_emp" ADD CONSTRAINT "fk_Department_emp_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "Employees" ("emp_no");
+
+ALTER TABLE "Department_emp" ADD CONSTRAINT "fk_Department_emp_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "Departments" ("dept_no");
+
+ALTER TABLE "Department_Manager" ADD CONSTRAINT "fk_Department_Manager_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "Departments" ("dept_no");
+
+ALTER TABLE "Department_Manager" ADD CONSTRAINT "fk_Department_Manager_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "Employees" ("emp_no");
+
